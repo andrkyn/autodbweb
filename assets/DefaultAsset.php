2c1e9b9d52292faf6e54bbcf8aba4ee006cac79e
andrkyn <rulex128@rambler.ru>
2018-02-14 10:37:45 +0200

    fix8: items OK

--- assets/DefaultAsset.php
+++ assets/DefaultAsset.php
@@ -1,27 +1,20 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
 
 namespace app\assets;
 
 use yii\web\AssetBundle;
 
 /**
- * Main application asset bundle.
- *
- * @author Andrkyn <andrhil14@gmail.com>
+ * @author andrkyn
  * @since 2.0
  */
-class AppAsset extends AssetBundle
+class DefaultAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
-        'css/style.css',
+        'css/style-list-prod.css',
         'css/font-awesome.min.css',
     ];
     public $js = [
